09930919a9bdbe7e1e9788aa078ca119410c3700
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-08-26 06:33:19 +0300

    Add Stack function declarations

--- src/data_structures/stack/stack.h
+++ src/data_structures/stack/stack.h
@@ -4,9 +4,26 @@
 namespace polish_notation::data_structures::stack {
 using size_t = decltype(sizeof(1));
 
+template <typename T>
+struct Node;
+
+template <typename T>
 class Stack {
    private:
     size_t size_;
+	Node<T>* topPtr_;
+
+	public:
+	Stack();
+	~Stack();
+
+	bool push(const Node<T>& item);
+	T pop();
+	T top() const;
+	void destroy();
+
+	bool isEmpty() const;
+	size_t size() const;
 };
 } // namespace polish_notation::data_structures::stack
 
