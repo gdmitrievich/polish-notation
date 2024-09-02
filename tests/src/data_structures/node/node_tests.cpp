7eccc1727fb26b9cee66d43bbc84c7cd3560a90f
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-09-02 13:48:04 +0300

    test: Add TwoParamsCtorTests in node_tests

---
+++ tests/src/data_structures/node/node_tests.cpp
@@ -0,0 +1,26 @@
+#include "polish_notation/data_structures/node/node.h"
+
+#include <gtest/gtest.h>
+
+using polish_notation::data_structures::node::Node;
+
+TEST(TwoParamsCtorTests, CopyEmptyObj) {
+    Node<int> empty;
+
+    Node<int> nonEmpty(empty.data, empty.next);
+
+    EXPECT_EQ(nonEmpty.data, 0);
+    EXPECT_EQ(nonEmpty.next, nullptr);
+}
+
+TEST(TwoParamsCtorTests, CopyNonEmptyObj) {
+    Node<int> tmp(5, new Node<int>(4));
+
+    Node<int> nonEmpty(tmp.data, tmp.next);
+
+    EXPECT_EQ(nonEmpty.data, 5);
+    EXPECT_NE(tmp.next, nonEmpty.next);
+    EXPECT_EQ(tmp.next->data, nonEmpty.next->data);
+    EXPECT_EQ(tmp.next->next, nonEmpty.next->next);
+    EXPECT_EQ(tmp.next->next->data, nonEmpty.next->next->data);
+}
