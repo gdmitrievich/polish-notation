4da6947b778028b705eade9479ed0676ac40599b
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-09-04 13:11:50 +0300

    test: Add EnqueueTests.EnqueueThreeItems in queue_tests

--- tests/src/data_structures/queue/queue_tests.cpp
+++ tests/src/data_structures/queue/queue_tests.cpp
@@ -16,3 +16,20 @@ TEST(EnqueueTests, EnqueueOneItem) {
     EXPECT_EQ(q.frontPtr()->next, nullptr);
     EXPECT_EQ(q.frontPtr(), q.backPtr());
 }
+
+TEST(EnqueueTests, EnqueueThreeItems) {
+    QueueTests<int> q;
+
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+
+    EXPECT_EQ(q.peek(), 1);
+    EXPECT_EQ(q.size(), 3);
+    EXPECT_EQ(q.isEmpty(), false);
+
+    EXPECT_EQ(q.backPtr()->data, 1);
+    EXPECT_EQ(q.frontPtr()->data, 3);
+    EXPECT_EQ(q.backPtr()->next->next, q.frontPtr());
+    EXPECT_NE(q.frontPtr(), q.backPtr());
+}
