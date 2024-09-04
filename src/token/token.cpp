504dad22d2b3c6acb159738a7de3038e6003474d
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-09-04 18:17:49 +0300

    style: Remove blank lines

--- src/token/token.cpp
+++ src/token/token.cpp
@@ -2,8 +2,6 @@
 
 namespace polish_notation::token {
 Token::Token(Token::Id i) : id(i), data() {}
-
 Token::Token(double num) : id(Token::Id::num), data(num) {}
-
 Token::Token(Token::Id binary, Token::Priority p) : id(binary), data(p) {}
-}
+} // namespace polish_notation::token
--- src/token/token_data.cpp
+++ src/token/token_data.cpp
@@ -4,4 +4,4 @@ namespace polish_notation::token {
 Token::Data::Data() : num() {}
 Token::Data::Data(double n) : num(n) {}
 Token::Data::Data(Priority p) : priority(p) {}
-}
+} // namespace polish_notation::token
