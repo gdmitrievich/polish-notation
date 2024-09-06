03f18d45d6d732c3c7636e0c7f388f3c93180887
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-09-06 08:23:01 +0300

    refactor: Add return quardian in getLineWithoutSpaces

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -15,17 +15,18 @@ using polish_notation::token::Token;
 string getLineWithoutSpaces(const string& line) {
     auto stn = std::string::npos;
 
+    if (line.empty() || line.find_first_not_of(" ", 0) == stn)
+        return string();
+
     string newLine;
-    if (!line.empty() && line.find_first_not_of(" ", 0) != stn) {
-        size_t l {}, r {};
-        do {
-            l = r;
-            l = line.find_first_not_of(" ", l);
-            r = line.find_first_of(" ", l + 1);
-            if (l != stn)
-                newLine.append(line.substr(l, r - l));
-        } while (r != stn && l != stn);
-    }
+    size_t l {}, r {};
+    do {
+        l = r;
+        l = line.find_first_not_of(" ", l);
+        r = line.find_first_of(" ", l + 1);
+        if (l != stn)
+            newLine.append(line.substr(l, r - l));
+    } while (r != stn && l != stn);
 
     return newLine;
 }
