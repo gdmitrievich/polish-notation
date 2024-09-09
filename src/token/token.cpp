bea377fed06fa44b4b86378d2ea46d11ee8f1157
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-09-09 09:44:30 +0300

    feat: Add Token::isFunction() member

--- src/token/token.cpp
+++ src/token/token.cpp
@@ -11,9 +11,14 @@ Token::Token(Token::Id i) : id(i) {
 }
 Token::Token(double num) : id(Token::Id::num), data(num) {}
 
-bool Token::isBinaryOperator() const {
+inline bool Token::isBinaryOperator() const {
     return isEqualToOneOfTheIdList(
         {t_id::plus, t_id::minus, t_id::mult, t_id::div});
+}
+
+inline bool Token::isFunction() const {
+    return isEqualToOneOfTheIdList(
+        {t_id::sin, t_id::cos, t_id::tan, t_id::ctg, t_id::sqrt, t_id::ln});
 }
 
 Token::Priority Token::getBinaryOperatorPriority() const {
