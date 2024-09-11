957cb05e867a679781ecad9dd1654847b0dcb038
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-09-11 11:18:02 +0300

    feat: Add tryCalculatePostfixTokenQueue func

--- src/polish_calculation/polish_calculation.cpp
+++ src/polish_calculation/polish_calculation.cpp
@@ -13,4 +13,20 @@ void replaceAllXWithNumInTokenQueue(Queue<Token>& q, double num) {
             q.enqueue(t);
     }
 }
+
+::std::pair<bool, double> tryCalculatePostfixTokenQueue(Queue<Token> qPostfix) {
+    Stack<Token> s;
+    bool isOk = true;
+    ::std::pair<bool, double> result(true, 0);
+
+    while (!qPostfix.isEmpty() && isOk)
+        isOk = tryCalculateRetrievedToken(qPostfix.dequeue(), s);
+
+    if (isOk && s.size() == 1)
+        result.second = s.pop().data.num;
+    else
+        result.first = false;
+
+    return result;
+}
 } // namespace polish_notation::polish_calculation
