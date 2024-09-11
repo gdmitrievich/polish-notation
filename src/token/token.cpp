c111cb5f4927c474003430b521c681fc2c334760
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-09-11 14:36:03 +0300

    refactor: Move inline definitions into .ipp file for token.h

--- src/token/token.cpp
+++ src/token/token.cpp
@@ -1,8 +1,5 @@
 #include "polish_notation/token/token.h"
 
-#include <algorithm>
-#include <initializer_list>
-
 namespace polish_notation::token {
 Token::Token() : id(Token::Id::x), data() {}
 Token::Token(Token::Id i) : id(i) {
@@ -10,20 +7,6 @@ Token::Token(Token::Id i) : id(i) {
         data.priority = getBinaryOperatorPriority();
 }
 Token::Token(double num) : id(Token::Id::num), data(num) {}
-
-inline bool Token::isBinaryOperator() const {
-    return isEqualToOneOfTheIdList(
-        {t_id::plus, t_id::minus, t_id::mult, t_id::div});
-}
-
-inline bool Token::isFunction() const {
-    return isEqualToOneOfTheIdList(
-        {t_id::sin, t_id::cos, t_id::tan, t_id::ctg, t_id::sqrt, t_id::ln});
-}
-
-inline bool Token::isNumOrX() const {
-    return isEqualToOneOfTheIdList({t_id::num, t_id::x});
-}
 
 Token::Priority Token::getBinaryOperatorPriority() const {
     Priority pr;
@@ -44,12 +27,5 @@ Token::Priority Token::getBinaryOperatorPriority() const {
     }
 
     return pr;
-}
-
-inline bool Token::isEqualToOneOfTheIdList(
-    const ::std::initializer_list<Token::Id>& idList) const {
-    return ::std::any_of(idList.begin(), idList.end(), [this](Token::Id tId) {
-        return id == tId;
-    });
 }
 } // namespace polish_notation::token
