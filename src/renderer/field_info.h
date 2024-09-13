0c37e0d32799143c333d202501a2410284657ec4
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-09-13 11:11:48 +0300

    style: Change indent

--- src/renderer/field_info.h
+++ src/renderer/field_info.h
@@ -5,12 +5,12 @@
 
 namespace polish_notation::renderer {
 struct FieldInfo {
-	int width;
-	int height;
-	::std::pair<double, double> domain; // Область определения.
-	::std::pair<double, double> codomain; // Область значений.
-	::std::pair<int, int> centerOfCoordinates;
+    int width;
+    int height;
+    ::std::pair<double, double> domain; // Область определения.
+    ::std::pair<double, double> codomain; // Область значений.
+    ::std::pair<int, int> centerOfCoordinates;
 };
-}
+} // namespace polish_notation::renderer
 
 #endif // FIELD_INFO_H
