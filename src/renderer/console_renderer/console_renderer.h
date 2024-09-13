8fbe694cc8d7732003fb3bb36713c59226b803cc
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-09-13 11:12:20 +0300

    feat: Add renderField func

---
+++ src/renderer/console_renderer/console_renderer.h
@@ -0,0 +1,10 @@
+#ifndef CONSOLE_RENDERER_H
+#define CONSOLE_RENDERER_H
+
+#include <vector>
+
+namespace polish_notation::renderer::console {
+void renderField(const ::std::vector<::std::vector<char>>& field);
+}
+
+#endif // CONSOLE_RENDERER_H
