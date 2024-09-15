c985f9770fe4b001bab9cea354c1ccc46ccb3bff
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-09-15 18:17:34 +0300

    refactor: Add throw operators in Queue enqueue and peek members

--- include/polish_notation/data_structures/queue/queue.h
+++ include/polish_notation/data_structures/queue/queue.h
@@ -16,25 +16,25 @@ class Queue {
     ::polish_notation::data_structures::node::Node<T>* backPtr_;
     ::polish_notation::utility::size_t size_;
 
-    void deleteNodeAndMoveBackPtrToNextNode();
+    void deleteNodeAndMoveBackPtrToNextNode() noexcept;
 
    protected:
-    const ::polish_notation::data_structures::node::Node<T>* frontPtr() const;
-    const ::polish_notation::data_structures::node::Node<T>* backPtr() const;
+    const ::polish_notation::data_structures::node::Node<T>* frontPtr() const noexcept;
+    const ::polish_notation::data_structures::node::Node<T>* backPtr() const noexcept;
 
    public:
-    Queue();
+    Queue() noexcept;
     Queue(const Queue& q);
     Queue<T> operator=(const Queue& q) = delete;
-    ~Queue();
+    ~Queue() noexcept;
 
     void enqueue(const T& item);
     T dequeue();
     T peek() const;
-    void destroy();
+    void destroy() noexcept;
 
-    bool isEmpty() const;
-    ::polish_notation::utility::size_t size() const;
+    bool isEmpty() const noexcept;
+    ::polish_notation::utility::size_t size() const noexcept;
 };
 } // namespace polish_notation::data_structures::queue
 
