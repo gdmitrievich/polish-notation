50c40184df614a074696316759ce624ccc10a84e
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-09-16 07:40:41 +0300

    feat: Add InvalidFunction exception

---
+++ include/polish_notation/exceptions/invalid_function/invalid_function.h
@@ -0,0 +1,29 @@
+#ifndef INVALID_FUNCTION
+#define INVALID_FUNCTION
+
+#include <stdexcept>
+#include <string>
+
+namespace polish_notation::exceptions {
+class InvalidFunction : public ::std::runtime_error {
+   public:
+	enum class ErrType : char {
+		LBraceNotFound,
+		RBraceNotFound,
+		OperatorsAreLessThenOperands,
+		FunctionWithoutArg,
+		BinaryOperatorWithoutTwoOperands
+	};
+
+	InvalidFunction(ErrType errType, const ::std::string& message) noexcept;
+
+	ErrType getErrType() const noexcept;
+
+   private:
+	ErrType errType_;
+};
+}
+
+namespace pn_e = polish_notation::exceptions;
+
+#endif // INVALID_FUNCTION
---
+++ src/exceptions/invalid_function/invalid_function.cpp
@@ -0,0 +1,15 @@
+#include "polish_notation/exceptions/invalid_function/invalid_function.h"
+
+#include "polish_notation/utility/utility.h"
+
+namespace polish_notation::exceptions {
+InvalidFunction::InvalidFunction(ErrType errType,
+                                 const ::std::string& message) noexcept :
+    errType_(errType),
+    runtime_error(message) {}
+
+
+InvalidFunction::ErrType InvalidFunction::getErrType() const noexcept {
+	return errType_;
+}
+} // namespace polish_notation::exceptions
