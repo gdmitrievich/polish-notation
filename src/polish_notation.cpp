181524f4aa919a3fbbb70f9d772aaebe79d8f7ce
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-09-18 09:37:57 +0300

    feat: Change polish_notation main logic

--- src/polish_notation.cpp
+++ src/polish_notation.cpp
@@ -1,51 +1,17 @@
-#include <cmath>
+#include "menu_data_container.h"
 #include <iostream>
-#include <string>
-#include <utility>
-#include <vector>
-
-#include "polish_notation/data_structures/queue/queue.h"
-#include "polish_notation/exceptions/invalid_field_info_exception/invalid_field_info_exception.h"
-#include "polish_notation/exceptions/invalid_function/invalid_function.h"
-#include "polish_notation/parser/parser.h"
-#include "polish_notation/renderer/console_renderer/console_renderer.h"
-#include "polish_notation/renderer/field_generator.h"
-#include "polish_notation/renderer/field_info.h"
-#include "polish_notation/shunting_yard_alg/shunting_yard_alg.h"
-#include "polish_notation/token/token.h"
-#include "user_err_mess_printer.h"
-#include "user_input.h"
-
-namespace pn = polish_notation;
-namespace pn_ds = polish_notation::data_structures;
 
 int main(void) {
-    std::string funcStr;
-    pn_ui::setFunctionStr(funcStr);
-
-    std::string funcLineWithoutSpaces =
-        pn::parser::getLineWithoutSpaces(funcStr);
-
-    try {
-        pn_ds::queue::Queue<pn::token::Token> qTokens;
-        if (!pn::parser::trySetTokenQueueFromStr(qTokens,
-                                                 funcLineWithoutSpaces))
-            throw ::pn_e::InvalidFunction(
-                ::pn_e::InvalidFunction::ErrType::InvalidToken,
-                "invalid_function: Invalid token.");
-
-        pn_ds::queue::Queue<pn::token::Token> postfixTokenQueue =
-            pn::shunting_yard_alg::convertInfixTokenQueueToPostfix(qTokens);
-
-        pn::renderer::FieldInfo fInfo;
-        pn_ui::setFieldInfo(fInfo);
-        pn::renderer::checkInputDataAndThrowExceptionIfNeeded(fInfo);
-        pn::renderer::console::renderField(
-            pn::renderer::getGeneratedField(postfixTokenQueue, fInfo));
-    } catch (const ::std::exception& e) {
-        pn_uemp::printErrMess(e);
-        exit(EXIT_FAILURE);
-    }
-
+	pn_r_c_mdc::MenuDataContainer menuDataContainer;
+	menuDataContainer.renderMenu();
+	char ch {};
+	do {
+		menuDataContainer.renderMenu();
+		menuDataContainer.renderField();
+        if (read(STDIN_FILENO, &ch, 1) == 0) {
+			std::cout << "continue" << std::endl;
+			continue;
+		}
+	} while(menuDataContainer.update(ch));
     return 0;
 }
